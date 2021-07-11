3ea5c121edb3dd04b717cf9b128ec5ef2b3f5c92
Andrei <i.andrei.abramov@gmail.com>
2021-07-11 14:39:46 +0300

    Переработана отрисовка изображений при загрузке. Доработана фитрация.

--- js/modules/filters.js
+++ js/modules/filters.js
@@ -1,48 +1,54 @@
 import { getRandomArrayItems, sortArrayByComments } from '../services/utils.js';
 import { NUMBER_OF_RANDOM_IMAGES } from '../settings/settings.js';
+import { cachedData, renderImages } from './render-images.js';
 
 const filtersSection = document.querySelector('.img-filters');
+const filtersForm = filtersSection.querySelector('.img-filters__form');
+const buttons = filtersForm.querySelectorAll('button.img-filters__button');
 
 export const showFilters = () => {
   filtersSection.classList.remove('img-filters--inactive');
 };
 
-export const setFiltersHandlers = (dataObj) => {
-  const filtersForm = filtersSection.querySelector('.img-filters__form');
-  const buttons = filtersForm.querySelectorAll('button.img-filters__button');
-
-  const applyFilter = (evt) => {
-    console.log(dataObj);
-    const filteredImages = {};
-    let filter = 'filter-default';
-
-    buttons.forEach((button) => {
-      button.classList.remove('img-filters__button--active');
-    });
-    if (evt.target.tagName === 'BUTTON') {
-      evt.target.classList.add('img-filters__button--active');
-      filter = evt.target.getAttribute('id');
-    }
-
-    if (filter === 'filter-default') {
-      return dataObj;
-    }
-
-    if (filter === 'filter-random') {
-      const randomImages = getRandomArrayItems(Object.values(dataObj), NUMBER_OF_RANDOM_IMAGES);
-      randomImages.forEach((image) => {
-        filteredImages[image.id] = image;
-      });
-      return filteredImages;
-    }
-
-    if (filter === 'filter-discussed') {
-      const discussedImages = sortArrayByComments(Object.values(dataObj), 'comments');
-      console.log(discussedImages);
-      return discussedImages;
-    }
-  };
-
-  filtersForm.addEventListener('click', applyFilter);
+const markActiveFilter = (evt) => {
+  buttons.forEach((button) => {
+    button.classList.remove('img-filters__button--active');
+  });
+  if (evt.target.tagName === 'BUTTON') {
+    evt.target.classList.add('img-filters__button--active');
+  }
 };
 
+const getFilteredData = (evt) => {
+  const dataArray = [...cachedData];
+  let filter = 'filter-default';
+  let filteredImages = dataArray;
+
+  if (evt.target.tagName === 'BUTTON') {
+    filter = evt.target.getAttribute('id');
+  }
+  if (filter === 'filter-random') {
+    filteredImages = getRandomArrayItems(dataArray, NUMBER_OF_RANDOM_IMAGES);
+  }
+  if (filter === 'filter-discussed') {
+    filteredImages = sortArrayByComments(dataArray);
+  }
+
+  return filteredImages;
+};
+
+const clearPictures = (evt) => {
+  const pictures = document.querySelectorAll('.picture');
+  if (evt.target.tagName === 'BUTTON') {
+    pictures.forEach((picture) => picture.remove());
+  }
+};
+
+export const setFiltersHandlers = () => {
+  filtersForm.addEventListener('click', (evt) => {
+    markActiveFilter(evt);
+    getFilteredData(evt);
+    clearPictures(evt);
+    renderImages(getFilteredData(evt));
+  });
+};
